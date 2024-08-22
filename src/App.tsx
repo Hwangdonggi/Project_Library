66726f176f0d3333c4e3e0d30bd143c8ff8aa4ed
donggi <donggi@naver.com>
2024-08-22 15:29:26 +0900

    logintwo

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Routing from './routing/Routing';
+import Login from './login/Login';
 
 function App() {
   return (
     <div className="App">
-      <Routing/>
+      {/* <Routing/> */}
+      <Login/>
      
     </div>
   );
--- src/routing/Routing.tsx
+++ src/routing/Routing.tsx
@@ -3,9 +3,9 @@ import { BrowserRouter, Router, Routes } from "react-router-dom"
 const Routing = () => {
     <BrowserRouter>
         <Routes>
-             <Route path ="/login" ></Route>
+             {/* <Route path ="/login" ></Route>
             <Route path= "/membership"></Route>
-            <Route path = "/search"></Route>
+            <Route path = "/search"></Route> */}
         </Routes>
     </BrowserRouter>    
 }
